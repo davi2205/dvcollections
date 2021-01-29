b758658fb6ababe47b87c896bdd7861e586d0e6e
davi2205 <danilo.davilla@gmail.com>
2021-01-29 10:04:58 -0300

    minor changes

--- doublelinkedlist/list.go
+++ doublelinkedlist/list.go
@@ -4,32 +4,65 @@ import "github.com/cheekybits/genny/generic"
 
 type Generic generic.Type
 
-type GenericListNode struct {
+type GenericNode struct {
 	Value      Generic
 	list       *GenericList
-	prev, next *GenericListNode
+	prev, next *GenericNode
 }
 
 type GenericList struct {
-	firstNode *GenericListNode
+	firstNode *GenericNode
 }
 
-func (g *GenericList) Empty() bool {
-	return g.firstNode == nil
+func (l *GenericList) Empty() bool {
+	return l.firstNode == nil
 }
 
-func (g *GenericList) Last() *GenericListNode {
-	if g.firstNode == nil {
+func (l *GenericList) LastNode() *GenericNode {
+	if l.firstNode == nil {
 		return nil
 	}
 
-	node := g.firstNode
+	node := l.firstNode
 	for ; node.next != nil; node = node.next {
 	}
 
 	return node
 }
 
-func (g *GenericList) Append(node *GenericListNode) {
-	// TODO: finish this.
+func (l *GenericList) InsertNodeAfter(node, nodeToInsert *GenericNode) bool {
+	if node.list != l {
+		return false
+	}
+	if nodeToInsert != nil {
+		return false
+	}
+
+	nodeToInsert.list = l
+	nodeToInsert.prev = node
+	nodeToInsert.next = node.next
+
+	if node.next != nil {
+		node.next.prev = nodeToInsert
+	}
+	node.next = nodeToInsert
+
+	return true
+}
+
+func (l *GenericList) AppendNode(node *GenericNode) bool {
+	return l.InsertNodeAfter(l.LastNode(), node)
+}
+
+func (l *GenericList) Append(value Generic) bool {
+	return l.AppendNode(&GenericNode{Value: value})
+}
+
+func (l *GenericList) FindNode(predicate func(*GenericNode) bool) *GenericNode {
+	for node := l.firstNode; node != nil; node = node.next {
+		if predicate(node) {
+			return node
+		}
+	}
+	return nil
 }
